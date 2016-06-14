3d5db8775f61b985961d79cfd17cd54c68333e1e
Dustin Wurtz <dtwurtz@gmail.com>
2016-06-13 19:07:21 -0700

    Edited Login Page

--- hunt-app/src/app.js
+++ hunt-app/src/app.js
@@ -2,12 +2,12 @@ import { MasterPage, IndexPage, LoginPage, RegistrationPage, ProfilePage } from 
 import ReactStormpath, { Router, HomeRoute, LoginRoute, AuthenticatedRoute } from 'react-stormpath';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { IndexRoute, Route, } from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+import { IndexRoute, Route } from 'react-router';
+import createHashHistory from 'history/lib/createHashHistory';
 
 ReactStormpath.init();
 ReactDOM.render(
-  <Router history={createBrowserHistory()}>
+  <Router history={createHashHistory()}>
     <HomeRoute path='/' component={MasterPage}>
     <IndexRoute component={IndexPage} />
     <LoginRoute path='/login' component={LoginPage} />
