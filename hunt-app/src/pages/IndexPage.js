32a92c46451ab138b4c3b05dd0269851aa3f2122
morgen-montgomery <mfett004@gmail.com>
2016-06-15 16:45:26 -0700

    pages are getting styled

--- hunt-app/src/pages/IndexPage.js
+++ hunt-app/src/pages/IndexPage.js
@@ -6,19 +6,7 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <div className="container">
-        <h2 className="text-center">Welcome!</h2>
-        <hr />
-        <div className="jumbotron">
-          <p>
-            <strong>To my React application!</strong>
-          </p>
-          <p>Ready to begin? Try these Stormpath features that are included in this example:</p>
-          <ol className="lead">
-            <li><Link to="/register">Registration</Link></li>
-            <li><LoginLink /></li>
-            <li><Link to="/profile">Custom Profile Data</Link></li>
-          </ol>
-        </div>
+
       </div>
     );
   }
