c273eff6d9e8ecc2e44f47063147938a9936b084
Jenni Bradstreet <jennic15@gmail.com>
2016-06-16 11:41:31 -0700

    Add h2 element to login page.

--- hunt-app/src/pages/LoginPage.js
+++ hunt-app/src/pages/LoginPage.js
@@ -7,35 +7,40 @@ export default class LoginPage extends React.Component {
   render() {
     return (
       <Row>
-        <Col m={6} s={12} class="auth-form">
-          <Card>
-            <h3> Login </h3>
-            <hr />
-            <form id="signin-form">
-              <Input m={12} label="First Name" />
-              <Input m={12} type="email" label="Email" />
-              <Input m={12} type="password" label="Password" />
-              <div>
-                <Button node='a' waves='light'><Icon right>file_cloud</Icon>login button</Button>
-              </div>
-            </form>
-          </Card>
-        </Col>
-        <Col m={6} s={12} class="auth-form">
-          <Card>
-            <h3> Sign Up </h3>
-            <hr />
-            <form id="signup-form">
-              <Input m={12} label="First Name" />
-              <Input m={12} type="email" label="Email" />
-              <Input m={12} type="password" label="Password" />
-              <div>
-                <Button node='a' waves='light'><Icon right>file_cloud</Icon>Sign Up button</Button>
-              </div>
-            </form>
-          </Card>
-        </Col>
-      </Row>
+        <div>
+          <h2> Account Access </h2>
+        </div>
+        <Row>
+          <Col m={6} s={12} class="auth-form">
+            <Card>
+              <h3> Login </h3>
+              <hr />
+              <form id="signin-form">
+                <Input m={12} label="First Name" />
+                <Input m={12} type="email" label="Email" />
+                <Input m={12} type="password" label="Password" />
+                <div>
+                  <Button node='a' waves='light'><Icon right>file_cloud</Icon>login button</Button>
+                </div>
+              </form>
+            </Card>
+          </Col>
+          <Col m={6} s={12} class="auth-form">
+            <Card>
+              <h3> Sign Up </h3>
+              <hr />
+              <form id="signup-form">
+                <Input m={12} label="First Name" />
+                <Input m={12} type="email" label="Email" />
+                <Input m={12} type="password" label="Password" />
+                <div>
+                  <Button node='a' waves='light'><Icon right>file_cloud</Icon>Sign Up button</Button>
+                </div>
+              </form>
+            </Card>
+          </Col>
+        </Row>
+      </Row>  
     );
   }
 }
