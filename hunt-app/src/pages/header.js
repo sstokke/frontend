84cb51b3cb0a85fb5be983f06683d6e263276141
Sean <sstokke17@gmail.com>
2016-06-17 11:57:52 -0700

    navbar fun

--- hunt-app/src/pages/header.js
+++ hunt-app/src/pages/header.js
@@ -6,7 +6,7 @@ import {Button, Icon, Row, Input, Col, Card, CardTitle, Navbar, NavItem} from 'r
 export default class Header extends React.Component {
   render() {
     return (
-      <Navbar brand="Scavng'r" left className="black navbar-fixed">
+      <Navbar brand="Scavng'r" left className="black">
         <NavItem href='/'> Home </NavItem>
         <NavItem href='/login'> Login | Sign Up </NavItem>
         <NavItem href='/profile'> Profile </NavItem>
